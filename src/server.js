b9189a5689241da02d580fb7dd191a843ef1560a
ptlergo <ptlergo@gmail.com>
2016-11-20 02:53:20 -0500

    path.join in server file for view

--- src/server.js
+++ src/server.js
@@ -5,13 +5,7 @@ const PORT_DEFAULT = 3000;
 const app = express();
 
 // Connect clientside views
-app.use(express.static('client/'));
-
-app.get('/hello', (req, res) => {
-
-    res.status(200).send('Hello, world!');
-
-});
+app.use(express.static(path.join(__dirname, '../client/')));
 
 const port = config.PORT || PORT_DEFAULT;
 
