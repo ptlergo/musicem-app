843b539144d1468c9ce338df5a250e6e12742a93
ptlergo <ptlergo@gmail.com>
2016-11-19 19:15:27 -0500

    engines in package update. eslint mocha added. mocha test sample pass. SUCCESS

--- test/__test.js
+++ test/__test.js
@@ -1,9 +1,13 @@
 const expect = require('chai').expect;
+const numTest = 1;
 
 describe('Sample Test', () => {
-  it('First test', (done) => {
 
-    expect(1).to.be(1);
+    it('First test', (done) => {
 
-  });
+        expect(numTest).to.be.equal(numTest);
+        done();
+
+    });
+
 });
