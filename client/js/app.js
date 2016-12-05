4680d869fa38b43e192abdce90efafe1754b9206
ptlergo <ptlergo@gmail.com>
2016-12-05 17:54:36 -0500

    main controller creating

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,22 @@
-angular.module('myApp', ['ui.router', 'ngAnimate', 'ngMaterial']);
+angular.module('myApp', ['ui.router', 'firebase', 'ngAnimate', 'ngMaterial']).
+  constant('FIREBASE_URL', 'https://facebooklogin-4a1ff.firebaseio.com').
+  config(($stateProvider) => {
+
+      const firebaseState = {
+          controller: 'firebaseuiController',
+          name: 'firebaseui',
+          templateUrl: '../views/firebaseui.html',
+          url: '/firebaseui',
+      };
+
+      const successState = {
+          controller: 'SuccessController',
+          name: 'success',
+          templateUrl: '../views/success.html',
+          url: '/success',
+      };
+
+      $stateProvider.state(firebaseState);
+      $stateProvider.state(successState);
+
+  });
