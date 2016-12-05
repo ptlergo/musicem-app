a10d4b3927ff20f3d9c1c0a52599f26bf89db3b4
ptlergo <ptlergo@gmail.com>
2016-12-04 22:32:25 -0500

    gulp sourcemap for sass added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const nodemon = require('nodemon');
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
 const sInput = './client/stylesheets/scss/*.scss';
 const sOutput = './client/css';
@@ -24,7 +25,9 @@ gulp.task('sass', () => {
 
     return gulp.
       src(sInput, ['sass']).
+      pipe(sourcemaps.init()).
       pipe(sass().on('error', sass.logError)).
+      pipe(sourcemaps.write()).
       pipe(gulp.dest(sOutput));
 
 });
