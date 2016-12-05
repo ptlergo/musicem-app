99a2459f19d94c0c143b3b180ec4fd612f51a999
ptlergo <ptlergo@gmail.com>
2016-12-04 21:52:07 -0500

    nodemon task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 const gulp = require('gulp');
+const nodemon = require('nodemon');
 
 gulp.task('default', () => {
+
     console.log('gulp tasking');
+
+});
+
+gulp.task('start', () => {
+
+    nodemon({
+        script: 'src/server.js',
+        ext: 'js'
+    });
 
 });
