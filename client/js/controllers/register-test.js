7e17c8fcca35cf4f60dede1c9f5c52a39cb7179a
ptlergo <ptlergo@gmail.com>
2017-01-06 14:21:02 -0500

    register-test controller

---
+++ client/js/controllers/register-test.js
@@ -0,0 +1,41 @@
+angular.
+  module('myApp').
+  controller('RegCtrl', RegCtrl);
+
+RegCtrl.$inject = ['$firebaseAuth'];
+
+/**
+  * @ngdoc method
+  * @name RegCtrl
+  * @methodOf module.RegCtrl
+  * @description
+  * User registration options using angularFire
+  *
+  * @returns {}
+*/
+function RegCtrl ($firebaseAuth) {
+
+    const vm = this;
+
+    const auth = $firebaseAuth();
+
+
+    vm.signIn = () => {
+
+        vm.firebaseUser = null;
+        vm.error = null;
+
+        auth.$signInAnonymously().then((firebaseUser) => {
+
+            vm.firebaseUser = firebaseUser;
+
+        }).
+        catch((error) => {
+
+            vm.error = error;
+
+        });
+
+    };
+
+};
