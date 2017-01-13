35c038a7fdc8057a85f72b8fd2df1bc7fbc20fa3
ptlergo <ptlergo@gmail.com>
2017-01-13 17:46:05 -0500

    logout() building with debug message

--- client/js/controllers/logout.js
+++ client/js/controllers/logout.js
@@ -21,50 +21,9 @@ function LogoutController (Auth, $state) {
         password: '',
     };
 
-    vm.login = () => {
+    vm.logout = () => {
 
-        firebase.auth().
-          signInWithEmailAndPassword(vm.user.email, vm.user.password).
-          then((auth) => {
-            console.log(auth);
-
-              $state.go('home');
-
-          }, (error) => {
-
-              vm.error = error;
-
-          });
-
-    };
-
-    vm.fblogin = () => {
-
-        const provider = new firebase.auth.FacebookAuthProvider();
-
-        console.log('facebook login clicked');
-        console.log(provider);
-
-        firebase.auth().signInWithPopup(provider).
-          then((result) => {
-            console.log(result);
-
-
-              const token = result.credential.accessToken;
-              const user = result.user;
-
-              const userProf = provider.addScope('public_profile');
-              console.log('user profile: ' + userProf);
-
-          }).
-          catch((error) => {
-
-              const errorCode = error.code;
-              const errorMessage = error.message;
-              const email = error.email;
-              const credential = error.credential;
-
-          });
+      console.log('logout clicked');
 
     };
 
