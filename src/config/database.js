6fd90b2791fddff72404bedc83e3d545406fa131
ptlergo <ptlergo@gmail.com>
2017-02-22 02:57:58 -0500

    db access code added and snapshot

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,14 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-config');
+const serviceAccount = require('./musicem-cd810-firebase-adminsdk-w4xy7-ea304edc7c.json');
 require('dotenv').config();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.databaseURL,
+});
+
+const db = admin.database();
+const ref = db.ref('restricted_access/secret_document');
+ref.once('value', (snapshot) => {
+  console.log(snapshot.val());
 });
