63b24019261af6dedd4cb3bca3a1a16dcefbd2f6
ptlergo <ptlergo@gmail.com>
2017-02-22 02:13:37 -0500

    firebase config in env file

--- src/server.js
+++ src/server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const firebase = require('firebase');
 const config = require('./config');
+require('dotenv').config();
+
+console.log(process.env.apiKey);
+
 
 const PORT_DEFAULT = 3000;
 
