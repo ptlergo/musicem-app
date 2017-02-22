7ba2091e6b982ee19d0f7137d185502ec2982584
ptlergo <ptlergo@gmail.com>
2017-02-22 01:47:42 -0500

    cors dependency required for back and front ends connect

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const config = require('./config');
 
 const PORT_DEFAULT = 3000;
