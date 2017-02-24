83097b65fa36d4f48cb347749fef0570e8409816
ptlergo <ptlergo@gmail.com>
2017-02-24 11:20:16 -0500

    gulp file cleanup. test json send to test route to test scraped data once made

--- gulpfile.js
+++ gulpfile.js
@@ -5,48 +5,43 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const gulpDocs = require('gulp-ngdocs');
 
-
 const sassSource = './client/stylesheets/scss/*.scss';
 const sassDest = './client/stylesheets/css';
 
 /* Compile node server with nodemon */
 gulp.task('start', () => {
-
-    nodemon({
-        ext: 'js',
-        script: 'src/server.js',
-    });
-
+  nodemon({
+    ext: 'js',
+    script: 'src/server.js',
+  });
 });
 
 /* Browser sync task */
 gulp.task('browser-sync', () => {
-
-    browserSync.init({
-      server: {
-        baseDir: './client',
-      },
-      files: './client',
-    });
-
+  browserSync.init({
+    server: {
+      baseDir: './client',
+    },
+    files: './client',
+  });
 });
 
 /* Compile annotations into docs */
 gulp.task('ngdocs', () => {
-
-    return gulp.
-      src('./src/*.js').
-      pipe(gulpDocs.process()).
-      pipe(gulp.dest('./docs'));
-
+  return gulp
+    .src('./src/*.js')
+    .pipe(gulpDocs.process())
+    .pipe(gulp.dest('./docs'));
 });
 
 /* Sass compile once then reload browser */
 gulp.task('sass', () => {
   return gulp.src(sassSource)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(sassDest))
-    .pipe(browserSync.reload({ stream: true }));
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(sassDest))
+        .pipe(browserSync.reload({
+          stream: true,
+        }));
 });
 
 /* Default task that watches Sass, html, and reloads browsersync for changes */
--- src/server.js
+++ src/server.js
@@ -4,18 +4,17 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const firebaseAdmin = require('firebase-admin');
 const config = require('./config');
-const fireConfig = require('./config/firebase-config');
 const serviceAccount = require('./config/musicem-cd810-firebase-adminsdk-w4xy7-ea304edc7c.json');
 
+const PORT_DEFAULT = 3000;
 
+// Firebase database access
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount),
   databaseURL: process.env.databaseURL,
 });
 
 const database = firebaseAdmin.database().ref();
-
-const PORT_DEFAULT = 3000;
 
 const app = express();
 
@@ -39,6 +38,16 @@ app.use('/users', users);
 
 app.get('/', (req, res) => {
   res.render('index.html');
+});
+
+const scraps = {
+  key1: 'value1',
+  key2: 'value1',
+  key3: 'value1',
+};
+
+app.get('/test', (req, res) => {
+  res.json(scraps);
 });
 
 const port = config.PORT || PORT_DEFAULT;
