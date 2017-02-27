eba740422d1e4a1974d01880de511587c2432f6d
ptlergo <ptlergo@gmail.com>
2017-02-27 16:20:08 -0500

    appRoute.js init for angular routes

---
+++ client/js/appRoutes.js
@@ -0,0 +1,13 @@
+/**
+ * @ngdoc method
+ * @name appRoutes
+ * @methodOf module.appRoutes
+ * @description
+ * define angular routes
+ *
+ * @returns {}
+ */
+angular.module('appRoutes', [])
+.config(['$routeProvider', '$locationProvider', ($routeProvider, $locationProvider) => {
+
+}]);
