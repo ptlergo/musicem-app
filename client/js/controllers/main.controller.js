3a5152b42123b816aeb285adea33f3c198ad6338
ptlergo <ptlergo@gmail.com>
2017-03-08 01:08:48 -0500

    maincontroller test function to post using api route. ISSUE: navbar overlay zindex. SUCCESS

--- client/js/controllers/main.controller.js
+++ client/js/controllers/main.controller.js
@@ -10,7 +10,10 @@
 function MainController($http) {
   const vm = this;
   vm.message = 'main controller welcome!';
-
+  vm.$http = $http;
+  vm.postM = () => {
+    vm.$http.post('http://localhost:8080/api/add', { msg: 'hello!' });
+  };
 }
 
 MainController.$inject = ['$http'];
