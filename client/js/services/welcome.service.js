bc3fc6c63a5c855e644d3118c6a71df7806f28c1
ptlergo <ptlergo@gmail.com>
2017-03-25 03:27:55 -0400

    built service for welcome without image url

--- client/js/services/welcome.service.js
+++ client/js/services/welcome.service.js
@@ -11,12 +11,37 @@ function welcomeService() {
   return {
     section: [
       {
-        tagline: 'tagline',
-        msg: 'msg',
+        tagline: 'Welcome to music’em!',
+        msg: `A content agregation application that
+              fetches your favorite music feeds
+              from the best curated brands for your
+              personal customizable needs.`,
+        image: 'logo',
+      },
+      {
+        tagline: 'Stay informed with music’em!',
+        msg: 'Discover new content in no time, and no effort.',
+      },
+      {
+        tagline: 'Discover! Fetch! Feed!',
+        msg: `There’s a lot of content out there.
+              Too much to waste your time digging through.
+              Let music’em find the diamonds in the rough.`,
+      },
+      {
+        tagline: 'Track any curated brands!',
+        msg: `We’ve taken the time to allow a select
+              amount of trusted brands. These brands are
+              the taste makers in the music industry. Our list
+              is maintained daily.`,
         image: '',
       },
       {
-        tagline: 'tagline 2',
+        tagline: 'Intuitive Features!',
+        msg: `Use powerful features such as
+              filters, content saving, sharing, and others
+              to personalize your own feed.`,
+        image: '',
       },
     ],
   };
